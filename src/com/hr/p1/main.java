82319fbd2a8b0a7338b2bca30394501e76e77a1f
Boiko Anton <boiko@kml.kh.ua>
2017-02-17 11:33:00 +0200

    Initial commit

---
+++ src/com/hr/p1/main.java
@@ -0,0 +1,15 @@
+package com.hr.p1;
+
+/**
+ * Created by BoikoAnton on 17.02.2017.
+ */
+public class main {
+    public static void main(String[] args) {
+        Object o = new Object();
+        for (int i = 0; i < 20; i++) {
+            if (i % 2 == 0) {
+                System.out.println("i = " + i);
+            }
+        }
+    }
+}
